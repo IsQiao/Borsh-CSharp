50d3f622432cb7d6aeee02c63123c22d1b778ca1
qiao <qxwxintu@gmail.com>
2021-02-03 00:17:52 +0800

    update

---
+++ Borsh/BorshConvert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Borsh
+{
+    public class BorshConvert
+    {
+        public static byte[] SerializeObject(object? value)
+        {
+            return SerializeObject(value);
+        }
+
+        public static byte[] SerializeObject(object? value, Type? type)
+        {
+            var jsonSerializer = BorshSerializer.CreateDefault(settings);
+            return SerializeObjectInternal(value, null, jsonSerializer);
+        }
+
+        public static byte[] SerializeObjectInternal(object? value, Type? type, BorshSerializer borshSerializer)
+        {
+            var memStream = new MemoryStream();
+
+            using (var binWriter = new BinaryWriter(memStream))
+            {
+                borshSerializer.Serialize(binWriter, value, type);
+            }
+
+            return memStream.GetBuffer();
+        }
+    }
+}
---
+++ Borsh/BorshSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Borsh
+{
+    public class BorshSerializer
+    {
+        public void Serialize(BinaryWriter binaryWriter, object? value, Type? objectType)
+        {
+            SerializeInternal(binaryWriter, value, objectType);
+        }
+    }
+}
